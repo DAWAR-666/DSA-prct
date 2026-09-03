5a330a1298824408e2cdcdedd0036a4008187fbf
PRATHAM DAWAR <dawarpratham8@gmail.com>
2026-09-03 17:27:51 +0530

    Time: 22 ms (26.70%) | Memory: 47.6 MB (32.21%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,42 +1,38 @@
-public class Solution {
+class Solution {
     public String minWindow(String s, String t) {
-        if (t.isEmpty()) return "";
-
-        Map<Character, Integer> countT = new HashMap<>();
-        Map<Character, Integer> window = new HashMap<>();
-        for (char c : t.toCharArray()) {
-            countT.put(c, countT.getOrDefault(c, 0) + 1);
+        if (t.isEmpty()||s.length()<t.length()) return "";
+        HashMap <Character,Integer>window=new HashMap<>();
+        HashMap <Character,Integer>count=new HashMap<>();
+        for(char c:t.toCharArray()){
+            count.put(c,count.getOrDefault(c,0)+1);
         }
-
-        int have = 0, need = countT.size();
-        int[] res = {-1, -1};
-        int resLen = Integer.MAX_VALUE;
-        int l = 0;
-
-        for (int r = 0; r < s.length(); r++) {
+        int l=0;
+        int[] range={-1,-1};
+        int minLength=Integer.MAX_VALUE;
+        int have=0;
+        
+        int need=count.size();
+        for(int r=0;r<s.length();r++){
             char c = s.charAt(r);
             window.put(c, window.getOrDefault(c, 0) + 1);
-
-            if (countT.containsKey(c) && window.get(c).equals(countT.get(c))) {
+            if (count.containsKey(c) && window.get(c).equals(count.get(c))) {
                 have++;
             }
-
-            while (have == need) {
-                if ((r - l + 1) < resLen) {
-                    resLen = r - l + 1;
-                    res[0] = l;
-                    res[1] = r;
+            while(have==need){
+                if(r-l+1<minLength){
+                    minLength=r-l+1;
+                    range[0]=l;
+                    range[1]=r;
                 }
-
-                char leftChar = s.charAt(l);
-                window.put(leftChar, window.get(leftChar) - 1);
-                if (countT.containsKey(leftChar) && window.get(leftChar) < countT.get(leftChar)) {
+                char leftChar=s.charAt(l);
+                window.put(leftChar,window.get(leftChar)-1);
+                if(count.containsKey(leftChar)&&window.get(leftChar)<count.get(leftChar)){
                     have--;
                 }
                 l++;
             }
         }
 
-        return resLen == Integer.MAX_VALUE ? "" : s.substring(res[0], res[1] + 1);
+        return minLength==Integer.MAX_VALUE?"":s.substring(range[0],range[1]+1);
     }
 }

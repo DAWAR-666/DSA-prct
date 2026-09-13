4dc7d1191442d082cd4919de9e009f1d008816a6
PRATHAM DAWAR <dawarpratham8@gmail.com>
2026-09-13 22:11:56 +0530

    Time: 806 ms (27.37%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select round(avg(order_date=customer_pref_delivery_date)*100,2)as immediate_percentage from delivery where (customer_id,order_date)in(select customer_id,min(d3.order_date)from delivery d3 group by d3.customer_id)
